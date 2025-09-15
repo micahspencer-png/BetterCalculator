c97451029b0b42ccb5177567c2966ae25cd24a66
micahspencer-png <micahspencer@isu.edu>
2025-09-14 20:36:38 -0600

    Set up program

--- BetterCalculator/Program.cs
+++ BetterCalculator/Program.cs
@@ -1,15 +1,26 @@
-﻿namespace BetterCalculator
+﻿using System.Net.Quic;
+
+namespace BetterCalculator
 {
     //Micah Spencer
     //RCET2265
     //Fall 2025
     //Better Calculator Program
-    //
+    //https://github.com/micahspencer-png/BetterCalculator.git
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            bool quit = false;
+            bool proceed;
+            string userInput;
+            int result;
+
+            do
+            {
+                Console.WriteLine("");
+
+            } while (quit == false);    
         }
     }
 }
