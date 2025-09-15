cd0265ca988e1b9624d7d9a80d4fa9d1245f0dc7
micahspencer-png <micahspencer@isu.edu>
2025-09-14 19:51:42 -0600

    Add project files.

---
+++ BetterCalculator/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace BetterCalculator
+{
+    //Micah Spencer
+    //RCET2265
+    //Fall 2025
+    //Better Calculator Program
+    //
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
